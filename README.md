d49baaa245a8a47cc9d875be6d8d4a7285a2bc42
Max <MMWestphal01@indianatech.net>
2026-01-16 14:06:25 -0500

    Initial commit - first project

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Python Learning Journey
+
+This repository contains my Python Programming excercises and projects
+
+## About Me
+- Name: Max Westphal
+-Course: CS1300
+-School: Indiana tech
---
+++ hello.py
@@ -0,0 +1,6 @@
+# My first Python program
+max = input ("What's your name? ")
+print(f"Hello, max! Welcome to Python Programming!")
+print("Python version check:")
+import sys
+print(sys.vresion)
